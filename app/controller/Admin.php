a42ec9bf3678e9b026e0f8729fdd1abac35eb4b8
ipipip1735 <ipipip1735@163.com>
2020-07-22 06:06:27 +0800

    Update admin update admin module

--- app/controller/Admin.php
+++ app/controller/Admin.php
@@ -11,44 +11,84 @@ namespace app\controller;
 
 use app\model\Book;
 use app\model\User;
+use think\facade\Request;
 use think\facade\View;
-use think\model\concern\TimeStamp;
+use think\facade\Session;
 
 class Admin extends \app\BaseController
 {
 
     public function index()
     {
-        return View::fetch('main');
-    }
+        if(!Session::has('user')) return redirect('/Admin/login');
 
-    public function show()
-    {
         $list = Book::order('id', 'desc')->select();
 
         View::assign('list', $list);
-        return View::fetch('show');
+        return View::fetch();
     }
 
     public function modify()
     {
+        if(!Session::has('user')) return redirect('/Admin/login');
         return View::fetch();
     }
 
 
-    public function AdminModify()
+    public function adminModify()
     {
+        if(!Session::has('user')) return redirect('/Admin/login');
+
         $data['name'] = 'admin';
         $data['password'] = $_POST['password'];
         $data['update_time'] = time();
 
-//        var_dump($data);
-
         $user = User::where('name', 'admin')->find();
-//        var_dump($user);
-
+        $user->password = MD5($data['password']);
+        $user->update_time = $data['update_time'];
+        $user->save();
 
         return View::fetch();
     }
+
+
+    public function login()
+    {
+        return View::fetch();
+    }
+
+    public function loginVerify()
+    {
+
+        if(!Request::has('userName', 'post')) return redirect('/Admin/login');
+
+        $user = User::where('name', Request::post('userName'))
+            ->where('password', MD5(Request::post('userPassword')))
+            ->findOrEmpty();
+
+        if ($user->isEmpty()) {
+            View::assign('error', '用户名或密码错误');
+            return View::fetch('error');
+        }
+
+
+        if(!Session::has('user')) Session::set('user', Request::post('userName'));
+
+
+        $list = Book::order('id', 'desc')->select();
+        View::assign('list', $list);
+        return View::fetch('index');
+    }
+
+
+    public function loginout()
+    {
+        if (Session::has('user')) {
+            Session::delete('user');
+        }
+        return redirect('/Admin/login');
+    }
+
+
 
 }
